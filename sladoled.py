31aba65503e9f7094f37dde042d7869f55a37a83
Marko <nocic.marko123@gmail.com>
2026-07-28 11:59:34 +0200

    Set up pygame and draw shapes in window
    Initialize pygame, create a window, and draw shapes.

--- sladoled.py
+++ sladoled.py
@@ -1,2 +1,12 @@
 import pygame as pg
-prozor = pg.display.set_mode((400, 400))
+pg.init()
+prozor=pg.display.set_mode((400,400))
+prozor.fill(pg.Color("White"))
+pg.draw.triangle(prozor, pg.Color("Yellow"), (166,333), (100,166), (233,166))
+pg.draw.circle(prozor, pg.Color(204, 222, 227), (166, 150), 33)
+pg.draw.circle(prozor, pg.Color(255, 102, 102), (200, 166), 33)
+pg.draw.circle(prozor, pg.Color(182, 219, 140), (100, 66), 33)
+pg.display.update()
+while pg.event.wait().type != pg.quit:
+  pass
+pg.quit()
