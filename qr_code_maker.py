379b61d5e466e78c70cb2de4dd5fc1c3eeb3f67e
Marko <nocic.marko123@gmail.com>
2026-08-05 17:19:39 +0200

    Update qr_code_maker.py

--- qr_code_maker.py
+++ qr_code_maker.py
@@ -1,1 +1,16 @@
-rgew
+from pathlib import Path
+import qrcode
+
+folder = Path("Qr codes")
+
+while True:
+    content = input().strip() 
+    if content.upper().strip() == "XXX":
+        print("See you soon, Take care :)")
+        break
+    else:
+        file_name = Path(f"{str(content.title())}.png")
+        file_path = folder / file_name
+        qr = qrcode.make(content)
+        qr.save(file_path)
+        print("Process text-to-QRcode is succesfully completed :)")
