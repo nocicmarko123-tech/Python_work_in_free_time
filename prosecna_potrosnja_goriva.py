a4046a30fc17c8bef82e94dd5321d44cbff39669
Marko <nocic.marko123@gmail.com>
2026-08-21 16:45:39 +0200

    Create prosecna_potrosnja_goriva.py

---
+++ prosecna_potrosnja_goriva.py
@@ -0,0 +1,8 @@
+def prosecna_potrosnja(*gorivo, kilometraza):
+    prosek = gorivo / (kilometraza / 100)
+    print(prosek)
+    return round(prosek, 2)
+
+kilometraza = int(input("Molimo unesite vasu kilometrazu(duzinu relacije): "))
+gorivo = int(input("Unesite ukupnu kolicinu potrosenog goriva: "))
+prosecna_potrosnja(gorivo, kilometraza)
